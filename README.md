8245728de03ae9e56e78b86809b7a3acf7d3b004
Sergio Livi <livi@i3s.unice.fr>
2016-03-08 13:47:28 +0100

    added cpu stats

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ You need:
 * the dependencies for the dependencies (scipy): `sudo apt-get install gfortran libopenblas-dev liblapack-dev`
 * Open vSwitch ~2.3.0
 * Docker ~1.10.2
+* iostat: `sudo apt-get install sysstat`
 
 Configure the environment with `source .env`; it will also install the dependencies. Exit the pyenv (and restart using you're good ole Python version) by running `deactivate`.
--- chain_ovs_iperf.py
+++ chain_ovs_iperf.py
@@ -19,7 +19,9 @@ def test(n_ovs, ovs_ovs_links, ovs_ns_links, parallelism=1, repetitions=1):
     script += m.get_script()
     script += ''
     script += 'umask 0000' #as the script will be run as root, this ensures that after you can play around as normal user ;)
-    script += 'TEST_DIR="results/chain_ovs_iperf" EXPORT_FILE="$TEST_DIR/{id}-{parallelism}-{n_ovs}-{ovs_ovs_links}-{ovs_ns_links}"'.format(**settings)
+    script += 'TEST_DIR="results/chain_ovs_iperf"'
+    script += 'EXPORT_FILE="$TEST_DIR/{id}-{parallelism}-{n_ovs}-{ovs_ovs_links}-{ovs_ns_links}"'.format(**settings)
+    script += 'CPU_FILE="${EXPORT_FILE}_cpu"'
     script += 'mkdir -p "$TEST_DIR"'
 
     #topology check
@@ -34,19 +36,22 @@ def test(n_ovs, ovs_ovs_links, ovs_ns_links, parallelism=1, repetitions=1):
     #script to run
     script += """
     ip netns exec x-ns1 iperf -s &>/dev/null & IPERF_PID=$!
+    trap "kill $IPERF_PID && sleep 1; opg_cleanup" EXIT
     for i in `seq {repetitions}`; do
         echo -n "Running iperf (with {parallelism} clients) ($i)... "
         sleep 1
-        csvline=$(ip netns exec x-ns2 timeout --signal=KILL 20 iperf -c 10.113.1.1 -y C -P {parallelism} | tail -n1)
+        (LC_ALL=C iostat -c 5 6 | awk 'FNR==3 {{ header = $0; print }} FNR!=1 && $0 != header && $0' >> $CPU_FILE) & IOSTAT_PID=$! # CPU monitoring
+        csvline=$(ip netns exec x-ns2 timeout --signal=KILL 45 iperf --time 30 --client 10.113.1.1 --reportstyle C --parallel {parallelism} | tail -n1)
         if [ "$csvline" ]; then
             measure=${{csvline##*,}}
             echo measured $(numfmt --to=iec --suffix=b/s $measure)
             echo $measure >> $EXPORT_FILE
+            sleep 5 #let the load decrease
         else
             echo error
         fi
+        wait $IOSTAT_PID
     done
-    kill $IPERF_PID && sleep 1
     """.format(**settings)
 
     script.run()
@@ -68,4 +73,6 @@ if __name__ == '__main__':
     else:
         #run the complete set!
         for (n_ovs, ovs_ovs_links, ovs_ns_links, parallelism) in itertools.product((0, 1, 2, 3, 5, 10, 20, 30, 50), ('patch', 'veth'), ('port', 'veth'), (1, 2, 4, 8, 12)):
+            if n_ovs is 0 and (ovs_ovs_links != 'veth' or ovs_ns_links != 'veth'):
+                continue
             test(n_ovs, ovs_ovs_links, ovs_ns_links, parallelism, 10)
