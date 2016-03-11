376037247fbf5007786f948a6e185fe3a310d1c9
Sergio Livi <livi@i3s.unice.fr>
2016-03-11 19:34:16 +0100

    splitted plots, cpu load, clickable on legend

--- chain_ovs_iperf_plot.py
+++ chain_ovs_iperf_plot.py
@@ -35,23 +35,37 @@ for f in p.glob('*/*/*'):
     with f.open() as file_handler:
         values = [int(line.rstrip()) for line in file_handler]
         r.throughput = mean_confidence(values)
+    with open(str(f)+'_cpu', 'r') as file_handler:
+        sums = [0.0]*6
+        length = 0
+        skip_line = False
+        for line in file_handler:
+            if skip_line or line.startswith('avg-cpu:'):
+                skip_line = not skip_line # if there's avg-cpu, we skip two lines
+                continue
+            row = map(float, line.split())
+            for i, v in enumerate(row):
+                sums[i] += v
+            length += 1
+        # %user %nice %system %iowait %steal %idle
+        r.cpu = tuple([v/length for v in sums]) #TODO: add confidence intervals!
     if r.n_ovs not in results:
         results[r.n_ovs] = []
     results[r.n_ovs].append(r)
 
 columns = sorted(results.keys())
-rows_ids = ('offloading', '4')
-rows = {}
-styles = {}
+rows_number = 3
+rows = []
+styles = []
 colors = {
     ('patch', 'port'): 'red',
     ('patch', 'veth'): 'blue',
     ('veth', 'port'): 'green',
     ('veth', 'veth'): 'black',
 }
-for id in rows_ids:
-    rows[id] = OrderedDict() # label of the serie => [ChainResult]
-    styles[id] = {} # label of the serie => **kwargs for matplotlib
+for i in range(rows_number):
+    rows.append(OrderedDict()) # label of the serie => [ChainResult]
+    styles.append({}) # label of the serie => **kwargs for matplotlib
 for (offloading, mss, parallelism, ovs_ovs_links, ovs_ns_links) in itertools.product(reversed(get_possibilities(results, 'offloading')), get_possibilities(results, 'mss'), get_possibilities(results, 'parallelism'), get_possibilities(results, 'ovs_ovs_links'), get_possibilities(results, 'ovs_ns_links')):
     links_type = (ovs_ovs_links, ovs_ns_links)
     label = '{} {} ({}offloading, mss: {})'.format('-'.join(links_type), parallelism, '' if offloading else 'no ', mss)
@@ -66,54 +80,94 @@ for (offloading, mss, parallelism, ovs_ovs_links, ovs_ns_links) in itertools.pro
             row.append(None)
     if tuple(set(row)) == (None,):
         continue
-    basestyle = { 'color': colors[links_type], 'linestyle': '-' }
+    basestyle = { 'color': colors[links_type], 'linestyle': '-', 'markersize': 7 }
     if offloading and mss == 'default':
-        rows['offloading'][label] = row
-        styles['offloading'][label] = basestyle.copy()
-        if parallelism is 1:
-            styles['offloading'][label]['marker'] = 'o'
-        elif parallelism is 2:
-            styles['offloading'][label]['marker'] = '^'
-        elif parallelism is 4:
-            styles['offloading'][label]['marker'] = 's'
-        elif parallelism is 8:
-            styles['offloading'][label]['marker'] = '+'
-        elif parallelism is 12:
-            styles['offloading'][label]['marker'] = 'x'
+        if parallelism <= 4:
+            row_id = 0
+            rows[row_id][label] = row
+            styles[row_id][label] = basestyle.copy()
+            if parallelism is 1:
+                styles[row_id][label]['marker'] = 'o'
+            elif parallelism is 2:
+                styles[row_id][label]['marker'] = '^'
+            elif parallelism is 4:
+                styles[row_id][label]['marker'] = 's'
+        if parallelism >= 4:
+            row_id = 1
+            rows[row_id][label] = row
+            styles[row_id][label] = basestyle.copy()
+            if parallelism is 4:
+                styles[row_id][label]['marker'] = 's'
+            elif parallelism is 8:
+                styles[row_id][label]['marker'] = '^'
+            elif parallelism is 12:
+                styles[row_id][label]['marker'] = 'o'
     if parallelism == 4:
-        rows['4'][label] = row
-        styles['4'][label] = basestyle.copy()
+        row_id = 2
+        rows[row_id][label] = row
+        styles[row_id][label] = basestyle.copy()
         if mss == 'default':
-            styles['4'][label]['marker'] = '+'
+            styles[row_id][label]['marker'] = '^'
         else:
-            styles['4'][label]['marker'] = 'x'
+            styles[row_id][label]['marker'] = 'o'
         if not offloading:
-            styles['4'][label]['linestyle'] = '--'
+            styles[row_id][label]['linestyle'] = '--'
 
-fig, axes = plt.subplots(2, 1)
-ax = {}
-ax['offloading'], ax['4'] = axes
-
-for id in ('offloading', '4'):
-    for label, row in rows[id].items():
-        cur_x = []
-        cur_y = []
-        cur_yerr=[]
+fig, axes = plt.subplots(rows_number, 2, sharex=True)
+lines_legend = {}
+for row_id, (ax_throughput, ax_cpu) in enumerate(axes):
+    lines = []
+    for label, row in rows[row_id].items():
+        x_values = []
+        throughput_values = []
+        throughput_error=[]
+        cpu_values = []
         for i, col in enumerate(columns):
             try:
                 r = row[i]
-                cur_y.append(r.throughput[0])
-                cur_yerr.append(r.throughput[1])
-                cur_x.append(col)
+                throughput_values.append(r.throughput[0])
+                throughput_error.append(r.throughput[1])
+                cpu_values.append(100-r.cpu[5]) # 100-idle
+                x_values.append(col)
             except:
                 pass
         kwargs = {}
-        if label in styles[id]:
-            kwargs = styles[id][label]
-        ax[id].errorbar(cur_x, cur_y, yerr=cur_yerr, label=label, **kwargs)
-    ax[id].set_xlabel('number of bridges')
-    ax[id].set_ylabel('throughput')
-    ax[id].legend(bbox_to_anchor=(1, 1), loc=2, fontsize='x-small')
+        if label in styles[row_id]:
+            kwargs = styles[row_id][label]
+        series_lines = []
+        line, two, three = ax_throughput.errorbar(x_values, throughput_values, yerr=throughput_error, label=label, **kwargs)
+        series_lines.extend((line,) + two + three)
+        series_lines += ax_cpu.plot(x_values, cpu_values, label=label, **kwargs)
+        lines.append(series_lines)
+    ax_throughput.set_xlabel('number of bridges')
+    ax_throughput.set_ylabel('throughput (b/s)')
+    ax_throughput.grid(True)
+    ax_cpu.set_xlabel('number of bridges')
+    ax_cpu.set_ylabel('cpu utilization (%)')
+    ax_cpu.grid(True)
+
+    legend = ax_cpu.legend(bbox_to_anchor=(1, 1), loc=2, fontsize='x-small')
+    for legline, origlines in zip(legend.get_lines(), lines):
+        legline.set_picker(5)  # 5 pts tolerance
+        lines_legend[legline] = origlines
+
+def onpick(event):
+    # on the pick event, find the orig line corresponding to the
+    # legend proxy line, and toggle the visibility
+    legline = event.artist
+    origlines = lines_legend[legline]
+    vis = not origlines[0].get_visible()
+    for origline in origlines:
+        origline.set_visible(vis)
+    # Change the alpha on the line in the legend so we can see what lines
+    # have been toggled
+    if vis:
+        legline.set_alpha(1.0)
+    else:
+        legline.set_alpha(0.2)
+    fig.canvas.draw()
+
+fig.canvas.mpl_connect('pick_event', onpick)
 
 plt.subplots_adjust(left=0.05, right=0.82, top=0.95, bottom=0.1)
 plt.show()
