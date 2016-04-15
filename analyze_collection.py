26035a309361e0f931af879ce9520e6afd822b53
Sergio Livi <livi@i3s.unice.fr>
2016-04-15 17:44:45 +0200

    added analysis for veth with iperf3

--- analyze_collection.py
+++ analyze_collection.py
@@ -49,13 +49,25 @@ def run_analysis(collection, action):
     if collection == 'veth':
         x_title = 'parallelism'
         x_axis = 'parallelism'  # key on result dict
-        db_query = [r for r in db if r['topology'] == 'direct_veth' and r['iperf_name'] == 'iperf2']
+        db_query = [r for r in db if r['topology'] == 'direct_veth']
 
         def row_info_fn(r):
-            key = "{}-{:05d}".format('z' if r['disable_offloading'] else 'a', r['packet_size'] if r['packet_size'] != 'default' else 0)
-            label = "{}offloading, pkt: {packet_size}".format('no ' if r['disable_offloading'] else '', **r)
+            zcpyaff_key = 'aa'
+            zcpyaff_label = ''
+            if r['iperf_name'] != 'iperf2':
+                zcpyaff_key = '{}{}'.format('z' if r['zerocopy'] else 'a', 'z' if r['affinity'] else 'a')
+                zcpyaff_label_list = []
+                for name in ('zerocopy', 'affinity'):
+                    if r[name]:
+                        zcpyaff_label_list.append(name)
+                zcpyaff_label = ', '.join(zcpyaff_label_list)
+            key = "{iperf_name: <7}-{}-{:05d}-{}".format('z' if r['disable_offloading'] else 'a', r['packet_size'] if r['packet_size'] != 'default' else 0, zcpyaff_key, **r)
+            label = "{iperf_name} {}offloading, pkt: {packet_size} {}".format('no ' if r['disable_offloading'] else '', zcpyaff_label, **r)
             return key, label
 
+        def grouping_fn(r):
+            iperf_names = ['iperf2', 'iperf3', 'iperf3m']
+            return (iperf_names.index(r['iperf_name']),)
         style_fn = lambda r, group_id: {'linestyle': '--' if r['disable_offloading'] else '-', }
 
     elif collection == 'ovs':
@@ -111,7 +123,35 @@ def run_analysis(collection, action):
     elif collection == 'iperf_cmp':
         x_title = 'number of namespaces'
         x_axis = 'chain_len'  # key on result dict
-        raise Exception('not implemented')
+        db_query = [r for r in db if
+                    r['topology'] == 'ns_chain' and
+                    r['chain_len'] in (2, 3, 5, 10) and
+                    r['ovs_ns_links'] == 'port' and
+                    r['packet_size'] == 'default'
+                    ]
+
+        def get_link_label(r):
+            link_label = 'direct-veth'
+            if r['use_ovs']:
+                link_label = 'ovs-{ovs_ns_links}'.format(**r)
+            return link_label
+
+        def row_info_fn(r):
+            key = "{iperf_name: <7}-{}-{parallelism:05d}-{}-{ovs_ns_links}-{}-{}".format('z' if r['disable_offloading'] else 'a', 'a' if r['use_ovs'] else 'z', 'z' if r['zerocopy'] else 'a', 'z' if r['affinity'] else 'a', **r)
+            label = "{iperf_name} {} {parallelism} ({}{}{})".format(get_link_label(r), '' if r['disable_offloading'] else 'offloading', ' zerocopy' if r['zerocopy'] else '', ' affinity' if r['affinity'] else '', **r)
+            return key, label
+
+        def grouping_fn(r):
+            groups = []
+            iperf_names = ['iperf2', 'iperf3', 'iperf3m']
+            iperf_plots = 2 if r['iperf_name'] == 'iperf2' else 5
+            if not r['disable_offloading'] and r['packet_size'] == 'default' and not r['zerocopy'] and not r['affinity']:
+                groups.append(iperf_names.index(r['iperf_name']) * iperf_plots)
+            if r['parallelism'] == 4:
+                groups.append(iperf_names.index(r['iperf_name']) * iperf_plots + 1)
+            if r['zerocopy'] or r['affinity']:
+                groups.append(iperf_names.index(r['iperf_name']) * iperf_plots + r['zerocopy'] * 2 + r['affinity'])
+            return groups
 
     cols, rows, rows_grouped = analyze.get_analysis_table(db_query, x_axis, row_info_fn, grouping_fn)
 
