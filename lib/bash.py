6225d602460e934ef097434bf3ce892eb4693d0f
Sergio Livi <livi@i3s.unice.fr>
2016-04-27 11:22:54 +0200

    beautifier

--- lib/bash.py
+++ lib/bash.py
@@ -1,30 +1,39 @@
 import subprocess
 
+
 class CommandBlock:
+
     @classmethod
     def root_check(cls):
         return cls() + 'if [ $EUID -ne 0 ]; then echo "root account required!"; exit 1; fi'
+
     def __init__(self):
         self.__commands = []
+
     def format(*args, **kwargs):
-        self = args[0] #must do this ugly trick in order to let you define self as a key in kwargs
+        self = args[0]  # must do this ugly trick in order to let you define self as a key in kwargs
         res = CommandBlock()
         for c in self.__commands:
             res += c.format(*args, **kwargs)
         return res
+
     def run(self):
         proc = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, universal_newlines=True)
         proc.communicate(str(self))
         return proc.returncode
+
     def __iter__(self):
         return self.__commands.__iter__()
+
     def __next__(self):
         return self.__commands.__next__()
+
     def __add__(self, arg):
         res = CommandBlock()
         res += self
         res += arg
         return res
+
     def __iadd__(self, arg):
         if arg is None:
             return self
@@ -32,16 +41,18 @@ class CommandBlock:
             self.__commands.append(arg)
             return self
         try:
-            self.__commands.extend(arg) #CommandBlock IS iterable, so this will also merge two CommandBlocks
+            self.__commands.extend(arg)  # CommandBlock IS iterable, so this will also merge two CommandBlocks
             return self
         except TypeError:
             pass
         return NotImplemented
+
     def __radd__(self, arg):
         res = CommandBlock()
         res += arg
         res += self
         return res
+
     def __str__(self):
         return "\n".join(self.__commands)
 
--- lib/topology.py
+++ lib/topology.py
@@ -1,8 +1,11 @@
-import re, wrapt
+import re
+import wrapt
 from .bash import CommandBlock
+
 
 class ConfigurationError(Exception):
     pass
+
 
 def add_comment(action):
     @wrapt.decorator
@@ -12,40 +15,50 @@ def add_comment(action):
         return _execute(*args, **kwargs)
     return wrapper
 
+
 class Entity:
     __shortname = 'x'
+
     def __init__(self):
         self.links = []
+
     def add_to(self, master):
         master.entities.append(self)
-        return self #chaining ;)
+        return self  # chaining ;)
+
     @add_comment('creating')
     def create(self):
         self.check_configuration()
         return CommandBlock()
+
     @add_comment('configuring')
     def configure(self):
         return CommandBlock()
+
     @add_comment('destroying')
     def destroy(self):
         return CommandBlock()
+
     def check_configuration(self):
         if self.name is None:
             raise ConfigurationError("name is missing")
+
     @property
     def entity_type_name(self):
         hierarchy = []
         ccls = self.__class__
         while ccls is not object:
             try:
-                hierarchy.append(getattr(ccls, '_'+ccls.__name__+'__shortname'))
+                hierarchy.append(getattr(ccls, '_' + ccls.__name__ + '__shortname'))
             except AttributeError:
                 pass
             ccls = ccls.__bases__[0]
         return '-'.join(reversed(hierarchy))
+
     @property
     def name(self):
         return self.__name
+
     @name.setter
     def name(self, value):
         self.__name = value
@@ -53,33 +66,41 @@ class Entity:
             self.name_id = re.search(r'(\d+)$', self.__name).group()
         except:
             self.name_id = None
+
     @property
     def description(self):
         return self.name
 
     def __str__(self):
         return self.description
+
     def __repr__(self):
         return self.__str__()
 
 
 class Netns(Entity):
     __shortname = 'ns'
+
     def __init__(self, name=None):
         super().__init__()
         self.name = name
         self.routes = []
+
     def add_route(self, destination, endpoint):
         self.routes.append((destination, endpoint))
+
     def create(self):
         return super().create() + "ip netns add {self.name}".format(self=self)
+
     def configure(self):
         cmds = CommandBlock()
         for r in self.routes:
-            cmds += "ip netns exec {self.name} ip route add "+r[0]+" via "+r[1].ip_address+" proto static"
+            cmds += "ip netns exec {self.name} ip route add " + r[0] + " via " + r[1].ip_address + " proto static"
         return super().configure() + cmds.format(self=self)
+
     def destroy(self):
         return super().destroy() + "ip netns delete {self.name}".format(self=self)
+
 
 class DockerContainer(Entity):
     pass
@@ -87,18 +108,23 @@ class DockerContainer(Entity):
 
 class OVS(Entity):
     __shortname = 'ovs'
+
     def __init__(self, name=None):
         super().__init__()
         self.name = name
+
     def create(self):
         return super().create() + "ovs-vsctl add-br {self.name}".format(self=self)
+
     def configure(self):
         return None
+
     def destroy(self):
         return super().destroy() + "ovs-vsctl del-br {self.name}".format(self=self)
 
 
 class Endpoint:
+
     @classmethod
     def get(cls, arg):
         if isinstance(arg, cls):
@@ -107,6 +133,7 @@ class Endpoint:
             return cls(arg)
         if isinstance(arg, tuple):
             return cls(*arg)
+
     def __init__(self, entity, ip_address=None, name=None):
         self.entity = entity
         self.name = name
@@ -120,15 +147,19 @@ class Endpoint:
             else:
                 self.ip_address = ip_address
                 self.ip_size = 24
+
     def __str__(self):
         return '{self.name} ({self.ip_address}/{self.ip_size})'.format(self=self)
+
     def __repr__(self):
         return self.__str__()
+
     def disable_offloading(self):
         return 'ethtool -K {self.name} tx off gso off sg off gro off'.format(self=self)
 
 
 class Link:
+
     @staticmethod
     def declare(e1, e2, link_type=None, **kwargs):
         e1 = Endpoint.get(e1)
@@ -144,7 +175,7 @@ class Link:
             else:
                 raise ConfigurationError('unrecognized type: {}'.format(link_type))
         if (type(e1.entity) is OVS and type(e2.entity) is Netns) or (type(e1.entity) is Netns and type(e2.entity) is OVS):
-            #make sure e1 is the OVS
+            # make sure e1 is the OVS
             if type(e1.entity) is Netns and type(e2.entity) is OVS:
                 e2, e1 = e1, e2
             if link_type is None:
@@ -166,18 +197,22 @@ class Link:
         self.disable_offloading = disable_offloading
         e1.entity.links.append(self)
         e2.entity.links.append(self)
+
     @add_comment('creating')
     def create(self):
         return CommandBlock()
+
     @add_comment('destroying')
     def destroy(self):
         return CommandBlock()
+
     @property
     def description(self):
         return "link between {self.e1.entity.name} and {self.e2.entity.name} of type {self.__class__.__name__} ({self.e1.name} to {self.e2.name})".format(self=self)
 
     def __str__(self):
         return self.description
+
     def __repr__(self):
         return self.__str__()
 
@@ -185,81 +220,97 @@ class Link:
     # links will be skipped EVEN IF they're of DIFFERENT TYPES
     # but they are NOT skipped if they have different ip addresses
     def __key(self):
-        return tuple( sorted([hash(self.e1), hash(self.e2)]) )
+        return tuple(sorted([hash(self.e1), hash(self.e2)]))
+
     def __hash__(self):
         return hash(self.__key())
+
     def __eq__(self, other):
         return self.__key() == other.__key()
+
     def __ne__(self, other):
         return not self.__eq__(other)
 
+
 class Link_OVS_OVS_veth(Link):
+
     def __init__(self, e1, e2, **kwargs):
         super().__init__(e1, e2, **kwargs)
+
     def assign_attributes(self):
         # veth names are limited to 15 chars(!)
         if self.e1.name is None:
             self.e1.name = 'veth-ovs-{e1.entity.name_id}-{e2.entity.name_id}'.format(**self.__dict__)
         if self.e2.name is None:
             self.e2.name = 'veth-ovs-{e2.entity.name_id}-{e1.entity.name_id}'.format(**self.__dict__)
+
     def create(self):
         self.assign_attributes()
         cmds = CommandBlock()
-        #create the links
+        # create the links
         cmds += "ip link add {e1.name} type veth peer name {e2.name}"
-        #configure one side
+        # configure one side
         cmds += "ovs-vsctl add-port {e1.entity.name} {e1.name}"
         cmds += "ip link set {e1.name} up"
         if self.disable_offloading:
             cmds += self.e1.disable_offloading()
-        #configure the other side
+        # configure the other side
         cmds += "ovs-vsctl add-port {e2.entity.name} {e2.name}"
         cmds += "ip link set {e2.name} up"
         if self.disable_offloading:
             cmds += self.e2.disable_offloading()
         return super().create() + cmds.format(**self.__dict__)
+
     def destroy(self):
         self.assign_attributes()
         return super().destroy() + "ip link delete {e1.name}".format(**self.__dict__)
 
+
 class Link_OVS_OVS_patch(Link):
+
     def __init__(self, e1, e2, **kwargs):
         super().__init__(e1, e2, **kwargs)
+
     def assign_attributes(self):
         if self.e1.name is None:
             self.e1.name = 'patch-{e2.entity.name}-{e1.entity.name_id}'.format(**self.__dict__)
         if self.e2.name is None:
             self.e2.name = 'patch-{e1.entity.name}-{e2.entity.name_id}'.format(**self.__dict__)
+
     def create(self):
         self.assign_attributes()
         cmds = CommandBlock()
         cmds += "ovs-vsctl add-port {e1.entity.name} {e1.name} -- set Interface {e1.name} type=patch options:peer={e2.name}"
         cmds += "ovs-vsctl add-port {e2.entity.name} {e2.name} -- set Interface {e2.name} type=patch options:peer={e1.name}"
         return super().create() + cmds.format(**self.__dict__)
+
     def destroy(self):
-        return None # destroyed by the bridge
+        return None  # destroyed by the bridge
 
 
 class Link_OVS_Netns_veth(Link):
     # e1 is the ovs, e2 is the netns
+
     def __init__(self, e1, e2, **kwargs):
         super().__init__(e1, e2, **kwargs)
+
     def assign_attributes(self):
         if self.e1.name is None:
             self.e1.name = 'v-ovs{e1.entity.name_id}-ns{e2.entity.name_id}'.format(**self.__dict__)
         if self.e2.name is None:
             self.e2.name = 'v-ns{e2.entity.name_id}-ovs{e1.entity.name_id}'.format(**self.__dict__)
+
     def create(self):
         self.assign_attributes()
         cmds = CommandBlock()
-        #create the links
+        # create the links
         cmds += "ip link add {e1.name} type veth peer name {e2.name}"
-        #configure ovs side
+        # configure ovs side
         cmds += "ovs-vsctl add-port {e1.entity.name} {e1.name}"
         cmds += "ip link set {e1.name} up"
         if self.disable_offloading:
             cmds += self.e1.disable_offloading()
-        #configure namespace side
+        # configure namespace side
         cmds += "ip link set {e2.name} netns {e2.entity.name}"
         cmds += "ip netns exec {e2.entity.name} ip link set dev {e2.name} up"
         if self.e2.ip_address is not None:
@@ -267,17 +318,22 @@ class Link_OVS_Netns_veth(Link):
         if self.disable_offloading:
             cmds += ("ip netns exec {e2.entity.name} " + self.e2.disable_offloading())
         return super().create() + cmds.format(**self.__dict__)
+
     def destroy(self):
         self.assign_attributes()
         return super().destroy() + "ip link delete {e1.name}".format(**self.__dict__)
 
+
 class Link_OVS_Netns_port(Link):
     # e1 is the ovs, e2 is the netns
+
     def __init__(self, e1, e2, **kwargs):
         super().__init__(e1, e2, **kwargs)
+
     def assign_attributes(self):
         if self.e2.name is None:
             self.e2.name = 'p-{e1.entity.name}-{e2.entity.name_id}'.format(**self.__dict__)
+
     def create(self):
         self.assign_attributes()
         cmds = CommandBlock()
@@ -289,31 +345,36 @@ class Link_OVS_Netns_port(Link):
         if self.disable_offloading:
             cmds += ("ip netns exec {e2.entity.name} " + self.e2.disable_offloading())
         return super().create() + cmds.format(**self.__dict__)
+
     def destroy(self):
-        return None # destroyed by the bridge
+        return None  # destroyed by the bridge
+
 
 class Link_Netns_Netns_veth(Link):
+
     def __init__(self, e1, e2, **kwargs):
         super().__init__(e1, e2, **kwargs)
+
     def assign_attributes(self):
         # veth names are limited to 15 chars(!)
         if self.e1.name is None:
             self.e1.name = 'veth-ns-{e1.entity.name_id}-{e2.entity.name_id}'.format(**self.__dict__)
         if self.e2.name is None:
             self.e2.name = 'veth-ns-{e2.entity.name_id}-{e1.entity.name_id}'.format(**self.__dict__)
+
     def create(self):
         self.assign_attributes()
         cmds = CommandBlock()
-        #create the links
+        # create the links
         cmds += "ip link add {e1.name} type veth peer name {e2.name}"
-        #configure one side
+        # configure one side
         cmds += "ip link set {e1.name} netns {e1.entity.name}"
         cmds += "ip netns exec {e1.entity.name} ip link set dev {e1.name} up"
         if self.e1.ip_address is not None:
             cmds += "ip netns exec {e1.entity.name} ip address add {e1.ip_address}/{e1.ip_size} dev {e1.name}"
         if self.disable_offloading:
             cmds += ("ip netns exec {e1.entity.name} " + self.e1.disable_offloading())
-        #configure the other side
+        # configure the other side
         cmds += "ip link set {e2.name} netns {e2.entity.name}"
         cmds += "ip netns exec {e2.entity.name} ip link set dev {e2.name} up"
         if self.e2.ip_address is not None:
@@ -321,14 +382,17 @@ class Link_Netns_Netns_veth(Link):
         if self.disable_offloading:
             cmds += ("ip netns exec {e2.entity.name} " + self.e2.disable_offloading())
         return super().create() + cmds.format(**self.__dict__)
+
     def destroy(self):
         self.assign_attributes()
         return super().destroy() + "ip netns exec {e1.entity.name} ip link delete {e1.name}".format(**self.__dict__)
 
 
 class Master:
+
     def __init__(self):
         self.entities = []
+
     def add(self, entity):
         self.entities.append(entity)
 
@@ -364,9 +428,11 @@ class Master:
         for obj in collection:
             commands += getattr(obj, fn)()
         return commands
+
     def setup(self):
         self.assign_attributes()
         return self.__get_commands(self.entities, 'create') + self.__get_commands(self.links, 'create') + self.__get_commands(self.entities, 'configure')
+
     def cleanup(self):
         return self.__get_commands(self.links, 'destroy') + self.__get_commands(self.entities, 'destroy')
 
