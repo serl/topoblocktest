711e75702a22aba96099d00d80948217132498a6
Sergio Livi <livi@i3s.unice.fr>
2016-05-05 19:46:44 +0200

    find and destroy stale experiments

--- generate_all.sh
+++ generate_all.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+shopt -s nullglob
+
+start_time="$(date +%s)"
+start_time=$((start_time - 10))  # ten seconds of error, just to be sure
 
 python iperf_veth_udp.py generate
 python iperf_veth_tcp.py generate
@@ -11,3 +15,33 @@ python iperf3m_chain_ns_tcp.py generate
 python iperf3m_veth_udp_packetsize.py generate
 
 python iperf3m_veth_udp_iptables.py generate
+
+function find_stale {
+    [ ! -z "$1" ] && delete=yes
+    count=0
+    for script_file in results/*.sh; do
+        edit_time=$(stat --format=%Y "$script_file")
+        if [ $edit_time -lt $start_time ]; then
+            count=$((count + 1))
+            if [ "$delete" ]; then
+                test_files="$(echo ${script_file%.*}.*)"
+                if [ -z "$test_files" ]; then
+                    continue
+                fi
+                rm $test_files
+            fi
+        fi
+    done
+    [ ! "$delete" ] && echo $count
+}
+
+count_stale=$(find_stale)
+[ "$count_stale" == 0 ] && exit
+
+echo
+echo "I found $count_stale stale experiments."
+read -p "Should I delete them? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    find_stale delete
+fi
