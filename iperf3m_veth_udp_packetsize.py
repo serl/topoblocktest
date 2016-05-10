ffc79678c63d13e287b9050b6b107269d95ac5eb
Sergio Livi <livi@i3s.unice.fr>
2016-05-10 11:36:56 +0200

    added affinity on packetsize tests

--- iperf3m_veth_udp_packetsize.py
+++ iperf3m_veth_udp_packetsize.py
@@ -14,8 +14,12 @@ class iperf3m_veth_udp_packetsize(collection.Collection):
         ('packet_size', tuple([2048 * (m + 1) - 29 for m in range(0, 32)])),
 
         ('disable_offloading', (False, True)),
+        ('affinity', (False, True)),
         ('parallelism', (1, 3, 4, 5, 6, 8, 12, 24)),
     ])
+
+    def generation_skip_fn(self, settings):
+        return settings['affinity'] and settings['disable_offloading']
 
     x_axis = 'packet_size'
     y_axes = ['throughput', 'packetput', 'cpu']
@@ -38,8 +42,13 @@ class iperf3m_veth_udp_packetsize(collection.Collection):
             12: 'red',
             24: 'yellow',
         }
+        linestyle = '-'
+        if r['disable_offloading']:
+            linestyle = '--'
+        elif r['affinity']:
+            linestyle = ':'
         return {
-            'linestyle': '--' if r['disable_offloading'] else '-',
+            'linestyle': linestyle,
             'color': colors[r['parallelism']],
         }
 
