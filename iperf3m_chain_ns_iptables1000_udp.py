587d2b421f10b756a24f09da4b01789330a1c890
Sergio Livi <livi@i3s.unice.fr>
2016-05-20 12:03:20 +0200

    1000 iptables rules spread on a chain

---
+++ iperf3m_chain_ns_iptables1000_udp.py
@@ -0,0 +1,53 @@
+import lib.collection as collection
+from collections import OrderedDict
+
+
+class iperf3m_chain_ns_iptables1000_udp(collection.Collection):
+    constants = {
+        'iperf_name': 'iperf3m',
+        'protocol': 'udp',
+        'topology': 'ns_chain_iptables',
+        'disable_offloading': False,
+        'zerocopy': False,
+        'affinity': False,
+        'use_ovs': False,
+        'ovs_ns_links': 'veth',
+        'packet_size': 32739,
+        'iptables_type': 'stateful',
+    }
+    variables = OrderedDict([
+        ('chain_len', (2, 3, 5, 11)),
+
+        ('parallelism', (1, 4, 6, 8)),
+        ('iptables_rules_len', (100, 250, 500, 1000)),
+    ])
+
+    def generation_skip_fn(self, settings):
+        return (settings['chain_len'] - 1) * settings['iptables_rules_len'] != 1000
+
+    x_axis = 'chain_len'
+    x_limits = (1.5, 11.5)
+    y_axes = ['throughput', 'packetput', 'cpu']
+    x_title = 'number of namespaces'
+
+    def analysis_row_label_fn(self, r):
+        return "{parallelism} UDP flows, {iptables_rules_len} {iptables_type} rules {}".format(', no offloading' if r['disable_offloading'] else '', **r)
+
+    def analysis_grouping_fn(self, r):
+        return (r['iptables_rules_len'],)
+
+    def plot_style_fn(self, r, group_id):
+        colors = {
+            1: 'black',
+            4: 'green',
+            6: 'red',
+            8: 'orange',
+        }
+        return {
+            'linestyle': '--' if r['iptables_type'] == 'stateless' else '-',
+            'color': colors[r['parallelism']],
+        }
+
+
+if __name__ == '__main__':
+    iperf3m_chain_ns_iptables1000_udp().parse_shell_arguments()
